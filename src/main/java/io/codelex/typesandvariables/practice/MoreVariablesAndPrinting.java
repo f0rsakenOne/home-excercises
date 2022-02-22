31b060470c23045ab76ac95d820780f800cd5632
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-02-22 21:24:47 +0200

    Finished excercise 2

--- src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
+++ src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
@@ -2,25 +2,30 @@ package io.codelex.typesandvariables.practice;
 
 public class MoreVariablesAndPrinting {
     public static void main(String[] args) {
-        String myName, myEyes, myTeeth, myHair;
-        int myAge, myHeight, myWeight;
 
-        myName = "Zed A. Shaw";
-        myAge = 35;
-        myHeight = 74;  // inches
-        myWeight = 180; // lbs
-        myEyes = "Blue";
-        myTeeth = "White";
-        myHair = "Brown";
+        String name;
+        String eyes;
+        String teeth;
+        String hair;
+        int age;
+        double height;
+        double weight;
 
-        System.out.println("Let's talk about " + myName + ".");
-        System.out.println("He's " + myHeight + " inches tall.");
-        System.out.println("He's " + myWeight + " pounds heavy.");
+
+        name = "Zed A. Shaw";
+        age = 35;
+        height = Math.round(74*2.54*100d)/100d;
+        weight = Math.round(180*0.45359237*100d)/100d;
+        eyes = "Blue";
+        teeth = "White";
+        hair = "Brown";
+
+        System.out.println("Let's talk about " + name + ".");
+        System.out.println("He's " + height + " centimeters tall.");
+        System.out.println("He's " + weight + " kilos heavy.");
         System.out.println("Actually, that's not too heavy.");
-        System.out.println("He's got " + myEyes + " eyes and " + myHair + " hair.");
-        System.out.println("His teeth are usually " + myTeeth + " depending on the coffee.");
-
-        System.out.println("If I add " + myAge + ", " + myHeight + ", and " + myWeight
-                + " I get " + (myAge + myHeight + myWeight) + ".");
+        System.out.println("He's got " + eyes + " eyes and " + hair + " hair.");
+        System.out.println("His teeth are usually " + teeth + " depending on the coffee.");
+        System.out.println("If I add " + age + ", " + height + ", and " + weight + " I get " + (age + height + weight) + ".");
     }
 }
