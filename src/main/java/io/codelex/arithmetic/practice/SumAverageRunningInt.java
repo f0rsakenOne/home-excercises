879d4bbc463cae5b1f83f84da16e5e588634ed24
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-02-24 21:28:10 +0200

    finished arithmetic exc3

--- src/main/java/io/codelex/arithmetic/practice/SumAverageRunningInt.java
+++ src/main/java/io/codelex/arithmetic/practice/SumAverageRunningInt.java
@@ -3,12 +3,16 @@ package io.codelex.arithmetic.practice;
 public class SumAverageRunningInt {
     public static void main(String[] args) {
         int sum = 0;
-        int average;
+        float average;
         int lowerBound = 1;
         int upperBound = 100;
-
+        int count = 0;
         for (int number = lowerBound; number <= upperBound; ++number) {
             sum += number;
+            count++;
         }
+        average = (float) sum/count ;
+        System.out.printf("The sum of %d to %d is %d%n",lowerBound,upperBound,sum);
+        System.out.printf("The average is %.1f%n",average);
     }
 }
