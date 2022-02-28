b964be8c09adabd4f9bb270ba6b6c9516c52de47
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-02-28 18:34:09 +0200

    didn't see that the output was little different xD

--- src/main/java/io/codelex/loops/exercises/AsciiFigure.java
+++ src/main/java/io/codelex/loops/exercises/AsciiFigure.java
@@ -2,15 +2,15 @@ package io.codelex.loops.exercises;
 
 public class AsciiFigure {
     public static void main(String[] args) {
-        final int size = 7;
+        final int size = 3;
         for (int i = 0; i < size ; i++) {
-            for (int j = size; j > i; j--) {
+            for (int j = size; j > i+1; j--) {
                 System.out.print("////");
             }
             for (int k =0;k<2*i;k++){
                 System.out.print("****");
             }
-            for (int j = size; j > i; j--) {
+            for (int j = size; j > i+1; j--) {
                 System.out.print("\\\\\\\\");
             }
             System.out.println();
