6d7c8af318520cf4e2a73ae79b4ffc26cf901e78
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-03-04 04:25:42 +0200

    finished hangman

--- src/main/java/io/codelex/arrays/exercises/Exercise8.java
+++ src/main/java/io/codelex/arrays/exercises/Exercise8.java
@@ -1,50 +1,94 @@
 package io.codelex.arrays.exercises;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Exercise8 {
+    //atvainojos par šo nekārtību
     static Random random = new Random();
-    private static final String[] words = {"important","spine","departure","concrete","intermediate"};
-    private static final String currentWord = words[random.nextInt(words.length)];
-    private static String[] foundLetters = new String[3];
+    private static final String[] words = {"important", "spine", "departure", "concrete", "intermediate", "integrity", "money","portrait","tray","point","top"};
+    private static String currentWord;
+    private static char[] foundLetters ;
+    private static StringBuilder misses;
 
     public static void main(String[] args) {
-        while ( !isWon() ) {
-            displayWord();
-            userInput();
-        }
-
-
+        newGame();
+        playGame();
     }
-    public static void displayWord(){
-        String[] letters = currentWord.split("");
-        System.out.print("Word : ");
-        for (String letter : letters){
-            if (isFound(letter)) {
-                System.out.print(letter+" ");
-            } else System.out.print("_ ");
 
+    private static void displayWord() {
+        System.out.print("Word : ");
+        for (char i : foundLetters) {
+            System.out.print(i + " ");
         }
         System.out.println();
     }
-    public static void userInput(){
+
+    private static void initSlashes() {
+        Arrays.fill(foundLetters, '_');
+    }
+
+    private static void displayMisses(){
+        System.out.println("Misses : "+ misses);
+    }
+
+    private static void guess() {
         Scanner keyboard = new Scanner(System.in);
-        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(foundLetters));
         System.out.print("Guess : ");
-        String key = keyboard.nextLine();
-        arrayList.add(key);
-        foundLetters = arrayList.toArray(foundLetters);
+        String letter = keyboard.next();
+        if (currentWord.contains(letter)){
+            int index = currentWord.indexOf(letter);
+            while ( index >= 0 ) {
+                foundLetters[index] = letter.charAt(0);
+                index = currentWord.indexOf(letter, index + 1);
+            }
+        } else {
+            misses.append(letter);
+        }
+
     }
 
-    public static boolean isFound(String input){
-        return false;
+    private static boolean checkWin() {
+        return currentWord.equals(String.valueOf(foundLetters));
+    }
+    private static boolean checkLoss() {
+        return misses.length()==3;
     }
 
-    public static boolean isWon(){
-        return false;
+    private static void newGame(){
+        currentWord = words[random.nextInt(words.length)];
+        foundLetters = new char[currentWord.length()];
+        misses = new StringBuilder();
+        initSlashes();
     }
+
+    private static void playGame(){
+
+        while ( !checkWin() && !checkLoss() ) {
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            displayWord();
+            displayMisses();
+            guess();
+        }
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        displayWord();
+        displayMisses();
+        if(checkWin()){
+            System.out.println("YOU GOT IT!");
+        } else System.out.println("You lost :(");
+        playAgain();
+    }
+
+    private static void playAgain(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Play \"again\" or \"quit\"? quit");
+        if(keyboard.next().equalsIgnoreCase("again")){
+            newGame();
+            playGame();
+        }
+
+    }
+
 
 }
