978310ae522be7b3d763e48a3a9a8ba06c5feb0a
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-03-14 14:48:01 +0200

    made some fixes

--- src/main/java/io/codelex/arrays/exercises/Exercise6.java
+++ src/main/java/io/codelex/arrays/exercises/Exercise6.java
@@ -4,19 +4,14 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Exercise6 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Random random = new Random();
         int[] numbers = new int[10];
-        for (int i =0 ; i<numbers.length;i++){
-            numbers[i] = 1+random.nextInt(100);
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = 1 + random.nextInt(100);
         }
         int[] numbers2 = numbers.clone();
-
-        for (int i =0 ; i<numbers.length;i++){
-            if (i==numbers.length-1){
-                numbers[i]=-7;
-            }
-        }
+        numbers[numbers.length - 1] = -7;
         System.out.println("Array1 : " + Arrays.toString(numbers));
         System.out.println("Array2 : " + Arrays.toString(numbers2));
     }
