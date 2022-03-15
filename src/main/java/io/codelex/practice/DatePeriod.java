4bf531fcdb3b3e09a0eb18a94a0acab4a08f2c48
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-03-15 22:52:13 +0200

    finished date exercises

--- src/main/java/io/codelex/practice/DatePeriod.java
+++ src/main/java/io/codelex/practice/DatePeriod.java
@@ -12,21 +12,42 @@ public class DatePeriod {
     }
 
     public DatePeriod intersection(DatePeriod secondPeriod) {
-        DatePeriod intersection = null;
-        if (secondPeriod.start.isBefore(this.end)) {
-            intersection = new DatePeriod(secondPeriod.start, this.end);
-        } else if (this.start.isBefore(secondPeriod.end)) {
-            intersection = new DatePeriod(this.start, secondPeriod.end);
+        if (isOverlapping(this.start, this.end, secondPeriod.start, secondPeriod.end)) {
+            if (isInsideAnother(this.start, this.end, secondPeriod.start, secondPeriod.end)) {
+                if (this.start.isBefore(secondPeriod.start)) {
+                    return new DatePeriod(secondPeriod.start, secondPeriod.end);
+                } else {
+                    return new DatePeriod(this.start, this.end);
+                }
+            }
+            if (isContainsAnother(this.start, this.end, secondPeriod.start, secondPeriod.end)) {
+                if (this.start.isBefore(secondPeriod.start)) {
+                    return new DatePeriod(secondPeriod.start, this.end);
+                } else {
+                    return new DatePeriod(this.start, secondPeriod.end);
+                }
+            }
         }
+        return null;
 
-        return intersection;
+    }
+
+    private boolean isOverlapping(LocalDate start1, LocalDate end1, LocalDate start2, LocalDate end2) {
+        return start2.isBefore(end1) && end2.isAfter(start1);
+    }
+
+    private boolean isInsideAnother(LocalDate start1, LocalDate end1, LocalDate start2, LocalDate end2) {
+        return ((start1.isBefore(start2) && end2.isBefore(end1)) || (start2.isBefore(start1) && end1.isBefore(end2)));
+    }
+
+    private boolean isContainsAnother(LocalDate start1, LocalDate end1, LocalDate start2, LocalDate end2) {
+        return (start2.isBefore(start1) && end2.isBefore(end1)) || (start1.isBefore(start2) && end1.isBefore(end2));
     }
 
     @Override
     public String toString() {
-        return
-                "start date = " + start.getDayOfMonth() + "." + start.getMonthValue() + "." + start.getYear() +
-                        ", end date = " + end.getDayOfMonth() + "." + end.getMonthValue() + "." + end.getYear()
+        return "start date = " + start.getDayOfMonth() + "." + start.getMonthValue() + "." + start.getYear() +
+                ", end date = " + end.getDayOfMonth() + "." + end.getMonthValue() + "." + end.getYear()
                 ;
     }
 }
--- src/main/java/io/codelex/practice/exc1.java
+++ src/main/java/io/codelex/practice/exc1.java
@@ -7,14 +7,6 @@ public class exc1 {
     public static void main(String[] args) {
         LocalDate date1 = LocalDate.of(1988, 6, 4);
         LocalDate date2 = LocalDate.of(2022, 6, 4);
-
-//        long hoursWorked = 0;
-//        while ( date1.isBefore(date2) ) {
-//            if (date1.getDayOfWeek() != DayOfWeek.SATURDAY && date1.getDayOfWeek() != DayOfWeek.SUNDAY) {
-//                hoursWorked += 8;
-//            }
-//            date1 = date1.plusDays(1);
-//        }
         long weeks = ChronoUnit.WEEKS.between(date1, date2);
         long hoursWorked = (ChronoUnit.DAYS.between(date1, date2) - (2 * weeks)) * 8;
         System.out.println(hoursWorked);
