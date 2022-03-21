8f19d291ad7a4d0ccf839ec323e7f1dfe336abd8
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-03-21 20:15:17 +0200

    did computer and parcel exercises

---
+++ src/main/java/io/codelex/oop/parcels/Parcel.java
@@ -0,0 +1,41 @@
+package io.codelex.oop.parcels;
+
+public class Parcel implements Validatable {
+    private final int maxLength = 300;
+    private final int minLength = 30;
+    private final float maxWeight = 30.0f;
+    private int xLength;
+    private int yLength;
+    private int zLength;
+    private float weight;
+    private boolean isExpress;
+
+    public Parcel(int xLength, int yLength, int zLength, float weight, boolean isExpress) {
+        this.xLength = xLength;
+        this.yLength = yLength;
+        this.zLength = zLength;
+        this.weight = weight;
+        this.isExpress = isExpress;
+    }
+
+    @Override
+    public boolean validate() {
+        if (xLength > maxLength || xLength < minLength) {
+            System.out.println("x length is to big/small");
+            return false;
+        } else if (yLength > maxLength || yLength < minLength) {
+            System.out.println("y length is to big/small");
+            return false;
+        } else if (zLength > maxLength || zLength < minLength) {
+            System.out.println("z length is to big/small");
+            return false;
+        } else if (!isExpress && weight < 30.0) {
+            System.out.println("weight is too small");
+            return false;
+        } else if (isExpress && weight < 15.0) {
+            System.out.println("weight is too small for express");
+            return false;
+        }
+        return true;
+    }
+}
