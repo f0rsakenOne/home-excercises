a853e3c025d2f7101164252a076d1f4a5a3c6c15
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-03-22 19:59:31 +0200

    added exceptions exercises

---
+++ src/main/java/io/codelex/exceptions/Exercise2.java
@@ -0,0 +1,12 @@
+package io.codelex.exceptions;
+
+public class Exercise2 {
+    public void methodA() throws ArithmeticException {
+    }
+
+    public void methodB() throws ArithmeticException {
+    }
+
+    public void methodC() throws ArithmeticException {
+    }
+}
