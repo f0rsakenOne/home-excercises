5e72ee9920a0b388978659358139292f245f18c3
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-03-25 01:59:05 +0200

    finished exercise 9

--- src/main/java/io/codelex/collections/practice/phonebook/PhoneDirectory.java
+++ src/main/java/io/codelex/collections/practice/phonebook/PhoneDirectory.java
@@ -1,19 +1,17 @@
 package io.codelex.collections.practice.phonebook;
 
-import java.util.Arrays;
+import java.util.TreeMap;
 
 public class PhoneDirectory {
-    private PhoneEntry[] data;
-    private int dataCount;
+    private TreeMap<Integer, PhoneEntry> data;
 
     public PhoneDirectory() {
-        data = new PhoneEntry[1];
-        dataCount = 0;
+        data = new TreeMap<>();
     }
 
     private int find(String name) {
-        for (int i = 0; i < dataCount; i++) {
-            if (data[i].name.equals(name)) {
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i).name.equals(name)) {
                 return i;
             }
         }
@@ -25,7 +23,7 @@ public class PhoneDirectory {
         if (position == -1) {
             return null;
         } else {
-            return data[position].number;
+            return data.get(position).number;
         }
     }
 
@@ -35,16 +33,12 @@ public class PhoneDirectory {
         }
         int i = find(name);
         if (i >= 0) {
-            data[i].number = number;
+            data.get(i).number = number;
         } else {
-            if (dataCount == data.length) {
-                data = Arrays.copyOf(data, 2 * data.length);
-            }
-            PhoneEntry newEntry = new PhoneEntry();  // Create a new pair.
+            PhoneEntry newEntry = new PhoneEntry();
             newEntry.name = name;
             newEntry.number = number;
-            data[dataCount] = newEntry;   // Add the new pair to the array.
-            dataCount++;
+            data.put(data.size(), newEntry);
         }
     }
 
