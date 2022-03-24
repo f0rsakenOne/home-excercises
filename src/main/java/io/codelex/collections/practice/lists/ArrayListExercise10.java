fa475fca59cd7a8320f537864fa45438b492fab4
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-03-24 22:48:40 +0200

    finished exercise  2

--- src/main/java/io/codelex/collections/practice/lists/ArrayListExercise10.java
+++ src/main/java/io/codelex/collections/practice/lists/ArrayListExercise10.java
@@ -15,8 +15,7 @@ public class ArrayListExercise10 {
         colors.add("Pink");
 
         System.out.println("Original array list: " + colors);
-        //TODO: Remove all elements from list
-        //...
+        colors.removeAll(colors);
 
         System.out.println("New array list: " + colors);
     }
--- src/main/java/io/codelex/collections/practice/lists/ArrayListExercise9.java
+++ src/main/java/io/codelex/collections/practice/lists/ArrayListExercise9.java
@@ -23,8 +23,9 @@ public class ArrayListExercise9 {
         secondList.add("Pink");
         System.out.println("List of second array: " + secondList);
 
-        //TODO: Write a Java program to join two array lists.
-        //...
+        List<String> joinedList = new ArrayList<>(firstList);
+        joinedList.addAll(secondList);
+        System.out.println(joinedList);
     }
 
 }
