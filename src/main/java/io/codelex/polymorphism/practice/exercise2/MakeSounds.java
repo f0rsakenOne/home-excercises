c389a5c64e04cd48b9e88cdc5abd43d66807b00c
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-03-28 18:08:58 +0300

    Finished exercise 2

--- src/main/java/io/codelex/polymorphism/practice/exercise2/MakeSounds.java
+++ src/main/java/io/codelex/polymorphism/practice/exercise2/MakeSounds.java
@@ -1,7 +1,17 @@
 package io.codelex.polymorphism.practice.exercise2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MakeSounds {
     public static void main(String[] arg) {
-        
+        Firework firework = new Firework();
+        Parrot parrot = new Parrot();
+        Radio radio = new Radio();
+        List<Sound> soundMakers = new ArrayList<>();
+        soundMakers.add(firework);
+        soundMakers.add(parrot);
+        soundMakers.add(radio);
+        soundMakers.forEach(Sound::playSound);
     }
 }
