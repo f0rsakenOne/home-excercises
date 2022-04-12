70a7825a4e0fb5802baf691422effbbdaa33da99
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-04-12 18:38:25 +0300

    made fixes

--- src/main/java/io/codelex/collections/practice/phonebook/PhoneTest.java
+++ src/main/java/io/codelex/collections/practice/phonebook/PhoneTest.java
@@ -1,12 +1,14 @@
 package io.codelex.collections.practice.phonebook;
 
 public class PhoneTest {
-    public static void main(String[] args) {
-        PhoneDirectory phoneDirectory = new PhoneDirectory();
-        phoneDirectory.putNumber("John", "25645456");
-        phoneDirectory.putNumber("Jihn", "284785612");
-        phoneDirectory.putNumber("Jehn", "47845653");
-        phoneDirectory.putNumber("Jehn", "54154565");
-        System.out.println(phoneDirectory.getNumber("Jehn"));
-    }
+
+  public static void main(String[] args) {
+    PhoneDirectory phoneDirectory = new PhoneDirectory();
+    phoneDirectory.putNumber("John", "25645456");
+    phoneDirectory.putNumber("Jihn", "284785612");
+    phoneDirectory.putNumber("Jehn", "47845653");
+    phoneDirectory.putNumber("Jehn", "54154565");
+    System.out.println(phoneDirectory.getPhoneEntry("Jehn"));
+
+  }
 }
