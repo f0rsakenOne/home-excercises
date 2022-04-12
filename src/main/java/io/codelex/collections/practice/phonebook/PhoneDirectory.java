c15ec0d66b3427d1025a44d7d9e346cc6dd41383
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-04-12 18:38:25 +0300

    made fixes

--- src/main/java/io/codelex/collections/practice/phonebook/PhoneDirectory.java
+++ src/main/java/io/codelex/collections/practice/phonebook/PhoneDirectory.java
@@ -3,43 +3,44 @@ package io.codelex.collections.practice.phonebook;
 import java.util.TreeMap;
 
 public class PhoneDirectory {
-    private TreeMap<Integer, PhoneEntry> data;
 
-    public PhoneDirectory() {
-        data = new TreeMap<>();
-    }
+  private TreeMap<Integer, PhoneEntry> data;
 
-    private int find(String name) {
-        for (int i = 0; i < data.size(); i++) {
-            if (data.get(i).name.equals(name)) {
-                return i;
-            }
-        }
-        return -1;
-    }
+  public PhoneDirectory() {
+    data = new TreeMap<>();
+  }
 
-    public String getNumber(String name) {
-        int position = find(name);
-        if (position == -1) {
-            return null;
-        } else {
-            return data.get(position).number;
-        }
+  private int find(String name) {
+    for (int i = 0; i < data.size(); i++) {
+      if (data.get(i).getName().equals(name)) {
+        return i;
+      }
     }
+    return -1;
+  }
 
-    public void putNumber(String name, String number) {
-        if (name == null || number == null) {
-            throw new IllegalArgumentException("name and number cannot be null");
-        }
-        int i = find(name);
-        if (i >= 0) {
-            data.get(i).number = number;
-        } else {
-            PhoneEntry newEntry = new PhoneEntry();
-            newEntry.name = name;
-            newEntry.number = number;
-            data.put(data.size(), newEntry);
-        }
+  public PhoneEntry getPhoneEntry(String name) {
+    int position = find(name);
+    if (position == -1) {
+      return null;
+    } else {
+      return data.get(position);
     }
+  }
+
+  public void putNumber(String name, String number) {
+    if (name == null || number == null) {
+      throw new IllegalArgumentException("name and number cannot be null");
+    }
+    int i = find(name);
+    if (i >= 0) {
+      getPhoneEntry(name).setNumber(number);
+    } else {
+      PhoneEntry newEntry = new PhoneEntry();
+      newEntry.setName(name);
+      newEntry.setNumber(number);
+      data.put(data.size(), newEntry);
+    }
+  }
 
 }
